ef52d9a992b9b65015be3c4b28bbcbcfcb410301
Alex Odorizzi <odorizzialex@yahoo.de>
2020-10-16 14:44:31 +0200

    added initial backend

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+require('dotenv').config({ path: './.env' });
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,12 +10,16 @@ app.use(cors());
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { NewUslParser: true, useCreateIndex: true }
-);
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true }
+    );
 const connection = mongoose.connection;
 connection.once('open', () => {
     console.log("MongoDB database connection established successfully");
 })
+
+const usersRouter = require('./routes/users');
+
+app.use('/users', usersRouter);
 
 app.listen(port, () => {
     console.log(`Server ist running on port: ${port}`);
