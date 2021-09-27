e077d0dcd796813ff41c648d9ffb7fe79a347f70
Alex Odorizzi <40853216+odorizziAlex@users.noreply.github.com>
2021-09-27 20:26:57 +0200

    added connection to socket.io and mongodb on same port
    new cluster created on mongodb atlas: ChatRoomCluster

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,29 @@
-const app = require('express')()
-const http = require('http').createServer(app)
-const io = require('socket.io')(http)
+// Notes
+// https://www.youtube.com/watch?v=5yTazHkDR4o&t=447s
+// 12:55
 
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// bodyparser middleware
+app.use(bodyParser.json());
+
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to mongodb
+mongoose.connect(db)
+    .then(()=>console.log("MongoDB connected..."))
+    .catch(err=>console.log(err))
+
+const PORT = process.env.PORT || 5000
+
+// implementing socket io
+const server = require('http').createServer(app)
+const io = require('socket.io')(server)
 
 io.on('connection', socket => {
     
@@ -12,12 +34,11 @@ io.on('connection', socket => {
     socket.on('userConnected', (name) => {
         io.emit('userConnected', name);
     })
-
+    
     socket.on('disconnect', () => {
         io.emit('userDisconnected');
     })
 })
 
-http.listen(5000, function() {
-    console.log('listening on port 5000');
-})
+//----------------
+server.listen(PORT, ()=>console.log(`Server started on port ${PORT}`))
